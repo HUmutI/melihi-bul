fde9d72f3dcee605f3d26f52fd6b102c0f093a37
HUmutI <huseyinumut2004@gmail.com>
2024-09-01 22:29:15 +0300

    voice feature added and bugs fixed

--- app/src/main/java/com/example/melihibul/MainActivity.kt
+++ app/src/main/java/com/example/melihibul/MainActivity.kt
@@ -5,15 +5,19 @@ import android.animation.AnimatorInflater
 import android.animation.AnimatorListenerAdapter
 import android.animation.AnimatorSet
 import android.annotation.SuppressLint
+import android.media.AudioManager
 import android.os.Bundle
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.example.melihibul.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import kotlin.random.Random
+import android.media.MediaPlayer
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,15 +25,22 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var melih = 0
     private var attempCount= 1
+    private var win = false
     private lateinit var imageViews: List<ImageView>
+    private var mMediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+        volumeControlStream = AudioManager.STREAM_MUSIC
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
         enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        val controller = WindowInsetsControllerCompat(window, window.decorView)
+        controller.hide(WindowInsetsCompat.Type.statusBars() or WindowInsetsCompat.Type.navigationBars())
+        controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -57,23 +68,34 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("ResourceType")
     private fun flipCard(index: Int) {
-        val imageView = imageViews[index]
 
-        // /res/anim içindeki animasyon dosyalarını al
-        val flipOutAnim = AnimatorInflater.loadAnimator(this, R.anim.card_flip_out) as AnimatorSet
-        val flipInAnim = AnimatorInflater.loadAnimator(this, R.anim.card_flip_in) as AnimatorSet
+        if (!win) {
+            val imageView = imageViews[index]
+
+            // /res/anim içindeki animasyon dosyalarını al
+            val flipOutAnim =
+                AnimatorInflater.loadAnimator(this, R.anim.card_flip_out) as AnimatorSet
+            val flipInAnim = AnimatorInflater.loadAnimator(this, R.anim.card_flip_in) as AnimatorSet
             flipOutAnim.setTarget(imageView)
             flipOutAnim.start()
 
             flipOutAnim.addListener(object : AnimatorListenerAdapter() {
                 override fun onAnimationEnd(animation: Animator) {
                     if (index == melih) {
+                        playSound2()
                         imageView.setImageResource(R.drawable.recep_turuncu_gomlek_nah)
-                        Snackbar.make(imageView,"MELİHİ $attempCount DENEMEDE BULDUN!", Snackbar.LENGTH_SHORT).show()
+                        Snackbar.make(
+                            imageView,
+                            "MELİHİ $attempCount DENEMEDE BULDUN!",
+                            Snackbar.LENGTH_SHORT
+                        ).show()
+                        win = true
 
                     } else {
+                        playSound()
                         imageView.setImageResource(R.drawable.recep_mavi_gomlek_nah)
-                        Snackbar.make(imageView,"Tekrar Dene",Snackbar.LENGTH_SHORT).show()
+                        imageView.isEnabled=false
+                        Snackbar.make(imageView, "Tekrar Dene", Snackbar.LENGTH_SHORT).show()
                         attempCount += 1
 
                     }
@@ -83,14 +105,17 @@ class MainActivity : AppCompatActivity() {
                 }
             })
 
+        }
     }
 
     @SuppressLint("ResourceType")
     private fun playAgain() {
         melih = Random.nextInt(0, 9)
         attempCount =1
+        win = false
 
         for (imageView in imageViews) {
+            imageView.isEnabled = true
             val flipOutAnim = AnimatorInflater.loadAnimator(this, R.anim.card_flip_out) as AnimatorSet
             val flipInAnim = AnimatorInflater.loadAnimator(this, R.anim.card_flip_in) as AnimatorSet
 
@@ -105,6 +130,46 @@ class MainActivity : AppCompatActivity() {
             flipOutAnim.start()
         }
         Snackbar.make(binding.root, "Oyun yeniden başladı!", Snackbar.LENGTH_SHORT).show()
+    }
+
+    fun playSound() {
+        mMediaPlayer?.release()
+        mMediaPlayer = MediaPlayer.create(this, R.raw.yok_yav)
+
+        mMediaPlayer?.let {
+            it.isLooping = false
+            it.start()
+            it.setOnCompletionListener {
+                it.release()
+                mMediaPlayer = null
+            }
+        } ?: run {
+            Snackbar.make(binding.root, "Error: Could not play sound", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    fun playSound2() {
+        mMediaPlayer?.release()
+        mMediaPlayer = MediaPlayer.create(this, R.raw.bu_olur)
+
+        mMediaPlayer?.let {
+            it.isLooping = false
+            it.start()
+            it.setOnCompletionListener {
+                it.release()
+                mMediaPlayer = null
+            }
+        } ?: run {
+            Snackbar.make(binding.root, "Error: Could not play sound", Snackbar.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (mMediaPlayer != null) {
+            mMediaPlayer!!.release()
+            mMediaPlayer = null
+        }
     }
 
 
